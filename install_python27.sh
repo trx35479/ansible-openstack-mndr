ddd253c8bce0de6db07a3484d394dae9e30fcff9
Ubuntu <ubuntu@packer.dev.unico.com.au>
2018-11-05 04:46:20 +0000

    Added bootstraping the MNDR App

--- install_python27.sh
+++ install_python27.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
-sudo dnf update -y
+#sudo dnf update -y
 sudo dnf -y install python 
 sudo dnf -y install ansible
--- prov_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IP_ADDRESS=$(echo `ifconfig ens3 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`)
-echo "The IP address to be configured is $IP_ADDRESS"
-
-URL_API="https://10.230.4.13:8443/mgmt/tm/ltm/pool/"
-
-PAYLOAD="'{ "name": "new-pool1", "members": [ { "name": "$IP_ADDRESS:3868", "description": "member5" } ] }'"
-
-curl -k -u admin:iops123! -H "Content-Type: application/json" -X POST -d $PAYLOAD $URL_API

b31e5ea4d0f5420df358ad8b0481e4350620ed21
Ubuntu <ubuntu@packer.dev.unico.com.au>
2018-11-15 02:42:58 +0000

    New yaml files for Heat Template

--- install_python.sh
+++ install_python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 #sudo dnf update -y
-sudo subscription-manager register --username=rowel.uchi@unico.com.au --password=Maxbella1
+sudo subscription-manager register --username=rowel.uchi@unico.com.au --password=$PASSWORD_RHN
 sudo subscription-manager attach
 sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo yum -y install python-pip
